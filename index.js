f38788f2f58eb3b620f4cdb8dea4d45e78139996
mrox <tuyen.appdev@gmail.com>
2020-04-08 10:47:48 +0700

    update view for mobile

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ import { ScatterGL } from "scatter-gl"
 import * as facemesh from '@tensorflow-models/facemesh'
 import * as tf from '@tensorflow/tfjs-core';
 import Stats from 'stats.js';
+import { round } from "prelude-ls";
 
 // import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
 
 // const scatterGL = new ScatterGL(document.querySelector('#scatter-gl-container'), { 'rotateOnStart': false, 'selectEnabled': true });
 const stats = new Stats();
-let model, video, mobile, videoWidth, videoHeight, canvas, ctx, scatterGLHasInitialized = false, scatterGL;
+let model, video, videoWidth, videoHeight, canvas, ctx, scatterGLHasInitialized = false, scatterGL;
+let preLog = document.querySelector('#log-container')
 const VIDEO_SIZE = 500;
+const mobile = isMobile();
 
+function isMobile() {
+    const isAndroid = /Android/i.test(navigator.userAgent);
+    const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    return isAndroid || isiOS;
+}
 
 async function setupCamera() {
     video = document.getElementById('video');
@@ -62,65 +70,36 @@ async function drawScatter(predictions) {
     let pointA = flattenedPointsData[362]
     let pointB = flattenedPointsData[133]
     let pointC = flattenedPointsData[62]
-    let pointD=   flattenedPointsData[291]
-    
-    flattenedPointsData = [
-        flattenedPointsData[1],
-        pointA,
-        pointB,
-        pointC,
-        pointD
-    ]
+    let pointD = flattenedPointsData[291]
 
     //Tính vector pháp tuyến của mặt phẳng mặt. từ 4 điểm 
     let vecAB = getVector(pointA, pointB)
     let vecAD = getVector(pointA, pointD)
     let vecBC = getVector(pointB, pointC)
 
-    let normalVec = sum(cross(vecAD,vecAB ), cross(vecBC, vecAB))
-    let horizontalAngle = Math.atan2(normalVec[0],normalVec[2]) * 180 / Math.PI
-    let verticalAngle = Math.atan2(normalVec[1],normalVec[2]) * 180 / Math.PI
-    console.log(`horizontalAngle: ${horizontalAngle}, verticalAngle: ${verticalAngle}`);
-    //
-    // var cPoint = [0, 0, 0]
-    // var minX = 1000000
-    // var maxX = -100000
+    let normalVec = sum(cross(vecAD, vecAB), cross(vecBC, vecAB))
+    let horizontalAngle = Math.atan2(normalVec[0], normalVec[2]) * 180 / Math.PI
+    let verticalAngle = Math.atan2(normalVec[1], normalVec[2]) * 180 / Math.PI
+    // console.log(`horizontalAngle: ${horizontalAngle}, verticalAngle: ${verticalAngle}`);
+    //show pre log
+    if(preLog) showPreLogs(horizontalAngle, verticalAngle)
 
-    // var minY = 100000
-    // var maxY = -100000
+    if (!mobile) {
+        const dataset = new ScatterGL.Dataset(flattenedPointsData);
 
-    // var minZ = 100000
-    // var maxZ = -100000
-
-    // flattenedPointsData.forEach(item => {
-    //     minX = Math.min(item[0], minX)
-    //     maxX = Math.max(item[0], maxX)
-
-    //     minY = Math.min(item[1], minY)
-    //     maxY = Math.max(item[1], maxY)
-
-    //     minZ = Math.min(item[2], minZ)
-    //     maxZ = Math.max(item[2], maxZ)
-
-    //     cPoint = [cPoint[0] + item[0], cPoint[1] + item[1], cPoint[2] + item[2]]
-    // })
-    // cPoint = [cPoint[0] / flattenedPointsData.length, cPoint[1] / flattenedPointsData.length, cPoint[2] / flattenedPointsData.length]
-    // cPoint = [(minX + maxX) / 2, (minY + maxY) / 2, (minZ + maxZ) / 2]
-
-    
-    // flattenedPointsData.push(cPoint)
-   
-    const dataset = new ScatterGL.Dataset(flattenedPointsData);
-   
-    if (!scatterGLHasInitialized) {
-        scatterGL.render(dataset);
-    } else {
-        scatterGL.updateDataset(dataset);
+        if (!scatterGLHasInitialized) {
+            scatterGL.render(dataset);
+        } else {
+            scatterGL.updateDataset(dataset);
+        }
+        scatterGLHasInitialized = true;
     }
-    scatterGLHasInitialized = true;
+
 }
 
-// async function drawNo
+function showPreLogs(horizontalAngle, verticalAngle){
+    preLog.innerHTML=`${round(horizontalAngle)}, ${round(verticalAngle)}`
+}
 
 
 async function main() {
@@ -165,16 +144,16 @@ async function main() {
 
 main()
 
-function getVector(A,B){
-    return [B[0] - A[0] , B[1] - A[1], B[2] - A[2] ]
+function getVector(A, B) {
+    return [B[0] - A[0], B[1] - A[1], B[2] - A[2]]
 }
 
 function cross(a, b) {
-    return [a[1] * b[2]  -  a[2]* b[1],   a[2] * b[0]  -  a[0]* b[2]   ,  a[0] * b[1]  -  a[1]* b[0]]
+    return [a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0]]
 }
 
-function sum(a,b) {
+function sum(a, b) {
 
-    return [a[0] + b[0], a[1] + b[1] ,a[2] + b[2] ]
+    return [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
 }
 
